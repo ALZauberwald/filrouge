c3a35f9809a8ce20122618e67224b8560799a530
NicolasR <50925924+NicolasR-github@users.noreply.github.com>
2022-07-12 23:08:28 +0200

    Add files via upload
    Téléchargement d'une amorce de classe, juste pour voir si elle sera visible d'ailleurs (ie pas en raw inexploitable)

---
+++ ProjetFilRouge/build/classes/com/lip6/entities/Formation.java
@@ -0,0 +1,25 @@
+package com.lip6.entities;
+/*Exemple de structure de catalogues de formations:
+
+*/
+
+
+
+public class Formation {
+	/*
+	 * Domaine, Thèmes T, Sous-thèmes S_t, Items(Intitulé, Mode(Mixte/Distan/Prés),
+	 * Codif, nbr de jours, certifiable ou non).
+	 */
+
+		private String Domaine;
+		private String T;
+		private String S_t;
+		private String Intitulé;
+		private String Mode;
+		private String Code;
+		private int duree;
+		private Boolean cert;
+		
+		
+
+}
