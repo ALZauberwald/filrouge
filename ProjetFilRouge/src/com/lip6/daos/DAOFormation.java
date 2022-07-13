91dfbf3bf46d51e1bb8e2e5bccaeba6ff8032eaa
Jérémy Caillé <48746426+ReisenSSBU@users.noreply.github.com>
2022-07-13 17:53:06 +0200

    Add files via upload

--- ProjetFilRouge/src/com/lip6/daos/DAOFormation.java
+++ ProjetFilRouge/src/com/lip6/daos/DAOFormation.java
@@ -10,9 +10,13 @@ import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import com.lip6.entities.Evaluation;
 import com.lip6.entities.Formation;
 import com.lip6.entities.Objectif;
 import com.lip6.entities.Prerequis;
+import com.lip6.entities.Salle;
+import com.lip6.entities.Session;
+import com.lip6.entities.TypeSession;
 import com.lip6.util.JpaUtil;
 
 
@@ -27,18 +31,14 @@ public class DAOFormation {
 			EntityTransaction tx = em.getTransaction();
 			
 			tx.begin();	
-				Objectif ob= new Objectif(formationobjectif);
-				
+				Objectif ob= new Objectif(formationobjectif);	
 				Prerequis pr= new Prerequis(formationprerequis);
 				
+				Formation newform= new Formation(formationname,ob,formationdetail, pr);
 				
-				Formation newform= new Formation(formationname,formationdetail);
-				
-				newform.getObjectifsFormation().add(ob);
-				
-				
-				newform.getPrerequisFormation().add(pr);
-				
+				//pour l'instant, lien onetoone à upgrader en lien one to many
+				ob.setFormation(newform);
+				pr.setFormation(newform);
 				
 				em.persist(newform);;
 			tx.commit();
@@ -52,6 +52,36 @@ public class DAOFormation {
 
 		return success;
 	}
+	
+	public boolean addSession(String nomSession, Float prixSession , String dateDebut , String lieuSession ,TypeSession typesession  ) {
+		
+			boolean success=false;
+			try {
+				EntityManager em=JpaUtil.getEmf().createEntityManager();	
+				EntityTransaction tx = em.getTransaction();
+				
+				tx.begin();	
+				Session se= new Session(nomSession, prixSession, dateDebut, lieuSession, typesession);
+				Salle sa = new Salle("truc boulevard","103");
+				Evaluation eva=new Evaluation("prof");
+				
+				se.setSalle(sa);
+				se.getEvaluations().add(eva);
+				
+				em.persist(se);
+								
+				tx.commit();
+				
+				em.close();
+				
+				success=true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return success;
+	}
+	
 	
 	
 }
