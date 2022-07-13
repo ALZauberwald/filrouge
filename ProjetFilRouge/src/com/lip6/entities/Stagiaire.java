731e96c2b6e1551eb4443b3cc048fd4b6a2456a9
NicolasR <50925924+NicolasR-github@users.noreply.github.com>
2022-07-13 17:30:26 +0200

    Add files via upload
    Nouvelles classes

---
+++ ProjetFilRouge/src/com/lip6/entities/Stagiaire.java
@@ -0,0 +1,24 @@
+package com.lip6.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Stagiaire extends Personne {
+	
+	@Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idStagiaire;
+
+	
+	public Stagiaire() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Stagiaire(String firstName, String lastName, String adr_electron, String adresse) {
+		super(firstName, lastName, adr_electron, adresse);
+		// TODO Auto-generated constructor stub
+	}
+
+}
