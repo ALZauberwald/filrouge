4be7ab9eb80a85a1ca0db9d91788bec85528fe60
Jeremy_maison <caillejeremy6@gmail.com>
2022-07-31 09:25:02 +0200

    premier test commit en local

---
+++ FilRouge/src/com/lip6/entities/Formation.java
@@ -0,0 +1,121 @@
+package com.lip6.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Formation {
+	@Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idFormation;	
+	private String nomFormation;
+	
+	@ManyToMany(cascade = CascadeType.PERSIST)
+	@JoinTable(name = "objectifs_par_formation",joinColumns = @JoinColumn(name = "id_formation"), inverseJoinColumns = @JoinColumn(name="id_objectif"))
+	private Set<Objectif> objectifsFormation = new HashSet<>();
+	
+	
+	private String detailFormation;
+	
+	@ManyToMany(cascade = CascadeType.PERSIST)
+	@JoinTable(name ="prerequis_par_formation", joinColumns=@JoinColumn(name = "id_formation"),inverseJoinColumns = @JoinColumn(name="id_prerequis"))
+	private Set<Prerequis> prerequisFormation= new HashSet<>();
+	
+	@OneToMany(cascade = CascadeType.PERSIST, mappedBy="formation")
+	private Set<Chapitre> chapitres= new HashSet<>();
+	
+	@ManyToMany( cascade=CascadeType.PERSIST)	
+	private Set<Theme> theme= new HashSet<>();
+	
+	public Formation() { }
+
+	public Formation(String nomFormation, String detailFormation) {
+		super();
+		this.nomFormation = nomFormation;
+		this.detailFormation = detailFormation;
+		
+	}
+
+
+	public int getIdFormation() {
+		return idFormation;
+	}
+
+	public void setIdFormation(int idFormation) {
+		this.idFormation = idFormation;
+	}
+
+	public String getNomFormation() {
+		return nomFormation;
+	}
+
+	public void setNomFormation(String nomFormation) {
+		this.nomFormation = nomFormation;
+	}
+
+
+
+	public Set<Objectif> getObjectifsFormation() {
+		return objectifsFormation;
+	}
+
+	public void setObjectifsFormation(Set<Objectif> objectifsFormation) {
+		this.objectifsFormation = objectifsFormation;
+	}
+
+	public String getDetailFormation() {
+		return detailFormation;
+	}
+
+	public void setDetailFormation(String detailFormation) {
+		this.detailFormation = detailFormation;
+	}
+
+	public Set<Prerequis> getPrerequisFormation() {
+		return prerequisFormation;
+	}
+
+	public void setPrerequisFormation(Set<Prerequis> prerequisFormation) {
+		this.prerequisFormation = prerequisFormation;
+	}
+
+	public Set<Chapitre> getChapitres() {
+		return chapitres;
+	}
+
+	public void setChapitres(Set<Chapitre> chapitres) {
+		this.chapitres = chapitres;
+	}
+
+	public Set<Theme> getTheme() {
+		return theme;
+	}
+
+	public void setTheme(Set<Theme> theme) {
+		this.theme = theme;
+	}
+
+	@Override
+	public String toString() {
+		return "Formation " + idFormation + "--> nom=" + nomFormation + ", objectifs="
+				+ objectifsFormation + ", detail=" + detailFormation + ", prerequis="
+				+ prerequisFormation ;
+	}
+	
+	
+	
+	
+	
+}
---
+++ FilRouge/src/com/lip6/servlets/CreerSessionServlet.java
@@ -0,0 +1,64 @@
+package com.lip6.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.lip6.entities.TypeSession;
+import com.lip6.services.FormationService;
+
+/**
+ * Servlet implementation class CreerSession
+ */
+@WebServlet("/CreerSessionServlet")
+public class CreerSessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreerSessionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nomSession =request.getParameter("nomSession");
+		String prixSession =request.getParameter("prix");
+		String dateDebut = request.getParameter("dateDebut");
+		String lieuSession =request.getParameter("lieuSession");
+		String typeSession = request.getParameter("typeSession");
+		String adresse = request.getParameter("adresse");
+		String nomSalle = request.getParameter("nomSalle");
+		
+		TypeSession type = TypeSession.INTER_ENTREPRISE;
+		if(typeSession.equals("inter entreprise"))
+			type= TypeSession.INTER_ENTREPRISE;
+		else if(typeSession.equals("intra entreprise"))
+			type= TypeSession.INTRA_ENTREPRISE;
+		else if(typeSession.equals("perso"))
+			type= TypeSession.PERSONNALISEE;
+		
+		
+		
+		FormationService formation= new FormationService();
+		formation.createSession(nomSession,Float.parseFloat(prixSession),dateDebut,lieuSession,type,adresse,nomSalle);
+		//redirection 
+		response.sendRedirect("index.html");
+	}
+
+}
